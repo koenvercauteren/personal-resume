5bbe92e17e2c59196cef212f6c2911ae2419f6a0
Koen <knvercauteren@gmail.com>
2025-06-12 17:30:40 +0200

    Technologies & tools + responsive image

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,14 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    responsiveImages: true,
+  },
+  image: {
+    experimentalLayout: "constrained",
+  },
   vite: {
     plugins: [tailwindcss()],
   },
-
   integrations: [react()],
 });
